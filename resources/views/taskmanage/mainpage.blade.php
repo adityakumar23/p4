1bc309232cac7d811d38ce86b0718c8f3e70ef2f
adityakumar@fas.harvard.edu <Aditya Kumar>
2016-05-09 12:37:28 -0400

    Correcting HTML validation errors

--- resources/views/taskmanage/mainpage.blade.php
+++ resources/views/taskmanage/mainpage.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.master')
 
-@if(!(Auth::check()))
-    <h1>Welcome to the Task Manager</h1>
 
-    <p>Don't have an account? <a href='/register'>Register here...</a></p>
+@section(content)
 
-    <p>Already have an account? <a href='/login'>Login here...</a></p>
+    @if(!(Auth::check()))
+        <h1>Welcome to the Task Manager</h1>
 
-@endif
+        <p>Don't have an account? <a href='/register'>Register here...</a></p>
+
+        <p>Already have an account? <a href='/login'>Login here...</a></p>
+
+    @endif
+    
+@stop
