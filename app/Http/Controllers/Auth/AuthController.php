a0cd4616cbdbe9705755304a2ff6a1ad71fe97ff
adityakumar@fas.harvard.edu <Aditya Kumar>
2016-05-12 13:53:12 -0400

    Finalizing things

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -54,7 +54,6 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            #'name'=> 'required|max:255', commented out Adityak 
             'first_name' => 'required|max:255',
             'last_name'=> 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
@@ -71,7 +70,7 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            #'name'=> $data['name'], commented out Adityak
+
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,31 +14,15 @@ Route::post('/login', 'Auth\AuthController@postLogin');
 #to display the register page
 Route::get('/register', 'Auth\AuthController@getRegister');
 
-#post registering the page to be displayed (THINK)
+#post registering the page to be displayed
 Route::post('/register', 'Auth\AuthController@postRegister');
 
-#after logout the user should be sent to the home page with a flash
-#message of being logged out
+#after logout the user should be sent to the home page
 
 Route::get('/logout', 'Auth\AuthController@logout');
 
 
-#test for checking login status of a user (comment out on production server)
-Route::get('/show-login-status', function() {
 
-    # You may access the authenticated user via the Auth facade
-    $user = Auth::user();
-
-    if($user) {
-        echo 'You are logged in.';
-        dump($user->toArray());
-    } else {
-        echo 'You are not logged in.';
-    }
-
-    return;
-
-});
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -53,6 +37,7 @@ Route::get('/show-login-status', function() {
 
 #default page displayed
 Route::get('/', 'TaskmanageController@getIndex');
+
 Route::get('/incomplete', 'TaskmanageController@getIncomplete');
 Route::get('/complete', 'TaskmanageController@getComplete');
 Route::get('/alltasks', 'TaskmanageController@getAlltasks');
@@ -72,57 +57,3 @@ Route::post('/delcomp', 'TaskmanageController@postDeletecomplete');
 
 Route::get('/delincomp/{id?}', 'TaskmanageController@getDeleteincomplete');
 Route::post('/delincomp', 'TaskmanageController@postDeleteincomplete');
-
-
-#Route::get('/incomplete', function () {
-#    return view('page for displaying incomplete tasks');
-#});
-#Route::get('/complete', function (){
-#    return view('page for displaying completed tasks');
-#});
-#Route::get('/alltasks', function(){
-#    return view('page for all tasks in different colors');
-#});
-#Route::get('/add', function(){
-#    return view('page for adding tasks');
-#});
-
-#Route::get('/edit', function(){
-#    return view('page for editing tasks');
-#});
-
-
-Route::get('/debug', function() {
-
-    echo '<pre>';
-
-    echo '<h1>Environment</h1>';
-    echo App::environment().'</h1>';
-
-    echo '<h1>Debugging?</h1>';
-    if(config('app.debug')) echo "Yes"; else echo "No";
-
-    echo '<h1>Database Config</h1>';
-    /*
-    The following line will output your MySQL credentials.
-    Uncomment it only if you're having a hard time connecting to the database and you
-    need to confirm your credentials.
-    When you're done debugging, comment it back out so you don't accidentally leave it
-    running on your live server, making your credentials public.
-    */
-    //print_r(config('database.connections.mysql'));
-
-    echo '<h1>Test Database Connection</h1>';
-    try {
-        $results = DB::select('SHOW DATABASES;');
-        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-        echo "<br><br>Your Databases:<br><br>";
-        print_r($results);
-    }
-    catch (Exception $e) {
-        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-    }
-
-    echo '</pre>';
-
-});
